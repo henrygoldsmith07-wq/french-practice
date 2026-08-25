4dff5df3cdba01fb49a8a2b7d7740576b9fb1807
henrygoldsmith07-wq <henrygoldsmith07@gmail.com>
2026-08-25 09:33:02 +0100

    chore: remove DSP debug scratch scripts from repo root

--- debug2.mjs
+++
@@ -1,5 +0,0 @@
-import { analyzeFrenchText } from './src/lib/frenchG2P.js';
-for (const word of ['trois', 'ils', 'chansons', 'vent', 'troi', 'troiss']) {
-  const r = analyzeFrenchText(word);
-  console.log(word, '->', JSON.stringify(r.words[0]?.phonemes), 'silent:', r.words[0]?.silentEnding);
-}
